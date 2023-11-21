484eba036c2da8623abf9b8b99d0ec5ee90c3e13
portal-da-qualidade-de-software <146787593+portal-da-qualidade-de-software@users.noreply.github.com>
2023-11-21 10:07:52 -0300

    Add files via upload

---
+++ javascript-mobile/design-de-software-mobile.js
@@ -0,0 +1,3 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    window.location.href = 'design-de-software-mobile.html'; // Substitua 'url-da-pagina-mobile.html' pelo caminho da sua página móvel
+}
---
+++ javascript-mobile/especificacao-de-requisitos-mobile.js
@@ -0,0 +1,3 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    window.location.href = 'especificacao-de-requisitos-mobile.html'; // Substitua 'url-da-pagina-mobile.html' pelo caminho da sua página móvel
+}
